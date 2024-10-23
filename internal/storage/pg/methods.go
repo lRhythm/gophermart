9b907913fe2375092d15505c28a4f1340a0a76c2
Калинин Александр Владимирович <kalinin_av@rusklimat.ru>
2024-10-23 22:39:10 +0300

    fix

--- internal/storage/pg/methods.go
+++ internal/storage/pg/methods.go
@@ -1,11 +1,14 @@
 package pg
 
+// Fox example: 1.2345 * mod → 12345; 12345 / mod = 1.2345.
+const mod = 10000
+
 // s2r - storage points (uint) to real points (float32).
 func s2r(u uint) float32 {
-	return float32(u) / 100
+	return float32(u) / mod
 }
 
 // r2s - real points (float32) to storage points (uint).
 func r2s(f float32) uint {
-	return uint(f * 100)
+	return uint(f * mod)
 }
